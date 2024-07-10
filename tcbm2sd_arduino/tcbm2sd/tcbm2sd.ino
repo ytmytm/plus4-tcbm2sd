690c00edd4d49493ed150ec710b244b4adc196e6
Maciej Witkowiak <mwitkowiak@gmail.com>
2024-07-10 21:54:58 +0200

    put all messages into PROGMEM and save a lot of RAM

--- tcbm2sd_arduino/tcbm2sd/tcbm2sd.ino
+++ tcbm2sd_arduino/tcbm2sd/tcbm2sd.ino
@@ -404,31 +404,31 @@ void set_error_msg(uint8_t error) {
   memset(output_buf, 0, sizeof(output_buf));
   switch (cbm_errno) {
     case 0:
-      strcpy((char*)output_buf, (const char*)"00, OK,00,00");
+      strcpy_P((char*)output_buf, (const char*)F("00, OK,00,00"));
       break;
     case 1:
-      strcpy((char*)output_buf, (const char*)"01, FILES SCRATCHED,01");
+      strcpy_P((char*)output_buf, (const char*)F("01, FILES SCRATCHED,01"));
       break;
     case 23:
-      strcpy((char*)output_buf, (const char*)"23, READ ERROR,00,00");
+      strcpy_P((char*)output_buf, (const char*)F("23, READ ERROR,00,00"));
       break;
     case 26:
-      strcpy((char*)output_buf, (const char*)"26, WRITE PROTECT ON,00,00");
+      strcpy_P((char*)output_buf, (const char*)F("26, WRITE PROTECT ON,00,00"));
       break;
     case 30:
-      strcpy((char*)output_buf, (const char*)"30, SYNTAX ERROR,00,00");
+      strcpy_P((char*)output_buf, (const char*)F("30, SYNTAX ERROR,00,00"));
       break;
     case 62:
-      strcpy((char*)output_buf, (const char*)"62, FILE NOT FOUND,00,00");
+      strcpy_P((char*)output_buf, (const char*)F("62, FILE NOT FOUND,00,00"));
       break;
     case 63:
-      strcpy((char*)output_buf, (const char*)"63, FILE EXISTS,00,00");
+      strcpy_P((char*)output_buf, (const char*)F("63, FILE EXISTS,00,00"));
       break;
     case 73:
-      strcpy((char*)output_buf, (const char*)"73, TCBM2SD 2024,00,00");
+      strcpy_P((char*)output_buf, (const char*)F("73, TCBM2SD BY YTM 2024,00,00"));
       break;
     default:
-      strcpy((char*)output_buf, (const char*)"99, UNKNOWN,00,00");
+      strcpy_P((char*)output_buf, (const char*)F("99, UNKNOWN,00,00"));
       break;
   }
 }
