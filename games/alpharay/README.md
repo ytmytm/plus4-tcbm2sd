5a613a3198f1c89c9659e2637b4d3989b789994c
Maciej Witkowiak <mwitkowiak@gmail.com>
2024-11-07 22:29:39 +0100

    Alpharay patched for TCBM protocol and TCBM fastload ; https://plus4world.powweb.com/software/Alpharay

---
+++ games/alpharay/README.md
@@ -0,0 +1,44 @@
+
+Alpharay patched for TCBM protocol (YaPe and TCBM2SD)
+by Maciej 'YTM/Elysium' Witkowiak
+
+# Source
+
+Alpharay IEC version from https://plus4world.powweb.com/software/Alpharay
+
+# Notes
+
+## Tools
+
+KickAss
+
+Exomizer
+
+c1541 (from VICE)
+
+## Makefile
+
+`make` prepares everything and generates new disk image `alpharay-tcbm.d81`.
+Note that it's not usable with emulators because it's not meant for 1581.
+`D81` is just data container, the loader uses TCBM protocol: like 1551/TCBM2SD.
+
+Some of the files can be moved to a `D64` image for debug.
+
+## `boot.bin`
+
+Decrunched `!!boot!!` file, starts from BASIC.
+
+## IOLib defaults
+
+Library init at `$6000`
+
+Fastloader at `$0200`
+
+Exomizer decruncher at `$0300-$04xx`.
+
+## Patches
+
+There is not enough space to fit full TCBM code for `startload` and `readbyte` (for decruncher) within 256 bytes.
+Since byte loader is now shorter part of `$036D-$0385` space is used for `startload`.
+
+`writebyte` protocol for filename is not supported, decruncher calls `$0210` with filename in X/Y (like `startload`).
---
+++ games/alpharay/alpharay-tcbm.asm
@@ -0,0 +1,82 @@
+
+.print "Assembling ALPHARAY-TCBM.PRG"
+.segmentdef Combined  [outPrg="alpharay-tcbm.prg", segments="Base,PatchDriveType,PatchLoader,PatchExo0,PatchExo", allowOverlap]
+
+.segment Base [start = $1001, max=$ffff]
+.var data = LoadBinary("boot.bin", BF_C64FILE)
+.fill data.getSize(), data.get(i)
+
+.label io_base = $0200
+.label io_tcbmoffs = $02FF
+
+// $0300-$0314
+.segment PatchExo0[min=$75f6,max=$75f6-$300+$314]
+.pc = $75F6
+.pseudopc $0300 {
+	jsr $0210
+	jmp $0315
+}
+
+// $036D-$0384
+.segment PatchExo[min=$7663,max=$7663-$36d+$384]
+.pc = $7663
+.pseudopc $036d {
+// patch data read from exodecruncher
+	jsr $0203
+	bcc $0365
+	bcs $0366
+// $0374
+// plug in part of startload so that we don't go over $0100 bytes for loader
+startload_part2:
+	jsr iolib.plus4tcbm_standard.TCBM_GETBYTE
+	sta.zp ptr
+	jsr iolib.plus4tcbm_standard.TCBM_GETBYTE
+	sta.zp ptr+1
+	ldy #0
+	rts
+}
+
+.segment PatchDriveType[]
+.pc = $6000 "Patch drivetype"
+
+	// XXX TCBM2SD / 1551 detect code could be here
+
+	ldx #0
+!:	lda $68ee,x
+	sta io_base,x
+	inx
+	bne !-
+
+	ldx #0
+!:	lda $75f6,x
+	sta $0300,x
+	inx
+	bne !-
+
+	ldx #$68
+!:	lda $76f6,x
+	sta $0400,x
+	dex
+	cpx #$ff
+	bne !-
+
+	lda $ae			// current device
+	cmp #8
+	beq !+
+	lda #$00		// drive 9
+	.byte $2c
+!:	lda #$30		// drive 8
+	sta io_tcbmoffs
+
+	jsr $020f		// init for compliance
+
+	lda #0
+	sta.zp io_bitbuff
+
+	clc
+	rts	
+
+.segment PatchLoader []
+.pc = $68ee "Patch loader for TCBM2SD"
+
+#import "tcbm2sdloaderbyte.asm"
